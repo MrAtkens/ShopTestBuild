bd514536596d96cac755903a01f36d7e533c14ea
MrAtkens <r.kaliaskar@mail.ru>
2019-09-27 16:10:13 +0600

    FrontBuildTest

---
+++ frontBuild/precache-manifest.2f8c3cbce09a52e9948d4eb44dc7265b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a551bf9dfed0a5f231ae0b9365d676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c5887e974e4cade7dd1",
+    "url": "/static/css/2.2c941c26.chunk.css"
+  },
+  {
+    "revision": "0c5887e974e4cade7dd1",
+    "url": "/static/js/2.8a62513f.chunk.js"
+  },
+  {
+    "revision": "30c84d94dd5e3a0f2b6d",
+    "url": "/static/js/main.369a1a1f.chunk.js"
+  },
+  {
+    "revision": "5a94c87a0d95f769d13b",
+    "url": "/static/js/runtime~main.cf89a219.js"
+  }
+]);
